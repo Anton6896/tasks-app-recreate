29482ff98021e261ab84967199aae6773949ce51
anton <anton-r@live.com>
2022-04-23 08:40:38 +0300

    init commit

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# tasks-app-recreate
+recreating tasks app 
---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+    return (
+        <main>
+            <p> testing </p>
+        </main>
+    );
+}
+
+export default App;
